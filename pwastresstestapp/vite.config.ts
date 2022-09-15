10f3071bd9d1cf0308b1062a81d3f082c34534c2
urapadmin <38838314+urapadmin@users.noreply.github.com>
2022-09-15 10:53:42 -0400

    moved icons to public to serve them unbundled

--- pwastresstestapp/vite.config.ts
+++ pwastresstestapp/vite.config.ts
@@ -7,7 +7,9 @@ export default defineConfig(({mode}) => {
     const env = loadEnv(mode, "env");
     return {
         base: '/',
-        build: {},
+        build: {
+            emptyOutDir: true,
+        },
         server: {
             https: true
         },
@@ -27,26 +29,26 @@ export default defineConfig(({mode}) => {
                     navigateFallback: 'index.html'
                 },
                 includeAssets: [
-                    'assets/android-icon-36x36.png',
-                    'assets/android-icon-48x48.png',
-                    'assets/android-icon-72x72.png',
-                    'assets/android-icon-96x96.png',
-                    'assets/android-icon-144x144.png',
-                    'assets/android-icon-192x192.png',
-                    'assets/apple-icon.png',
-                    'assets/apple-icon-57x57.png',
-                    'assets/apple-icon-60x60.png',
-                    'assets/apple-icon-72x72.png',
-                    'assets/apple-icon-76x76.png',
-                    'assets/apple-icon-114x114.png',
-                    'assets/apple-icon-120x120.png',
-                    'assets/apple-icon-144x144.png',
-                    'assets/apple-icon-152x152.png',
-                    'assets/apple-icon-180x180.png',
-                    'assets/apple-icon-precomposed.png',
-                    'assets/favicon-16x16.png',
-                    'assets/favicon-32x32.png',
-                    'assets/favicon-96x96.png'],
+                    'android-icon-36x36.png',
+                    'android-icon-48x48.png',
+                    'android-icon-72x72.png',
+                    'android-icon-96x96.png',
+                    'android-icon-144x144.png',
+                    'android-icon-192x192.png',
+                    'apple-icon.png',
+                    'apple-icon-57x57.png',
+                    'apple-icon-60x60.png',
+                    'apple-icon-72x72.png',
+                    'apple-icon-76x76.png',
+                    'apple-icon-114x114.png',
+                    'apple-icon-120x120.png',
+                    'apple-icon-144x144.png',
+                    'apple-icon-152x152.png',
+                    'apple-icon-180x180.png',
+                    'apple-icon-precomposed.png',
+                    'favicon-16x16.png',
+                    'favicon-32x32.png',
+                    'favicon-96x96.png'],
                 manifest: {
                     name: 'Kiosk PWAStressTestApp',
                     short_name: 'PWAStressTestApp',
@@ -54,37 +56,37 @@ export default defineConfig(({mode}) => {
                     theme_color: '#ffffff',
                     icons: [
                         {
-                            "src": "assets/android-icon-36x36.png",
+                            "src": "android-icon-36x36.png",
                             "sizes": "36x36",
                             "type": "image/png",
                             "density": "0.75"
                         },
                         {
-                            "src": "assets/android-icon-48x48.png",
+                            "src": "android-icon-48x48.png",
                             "sizes": "48x48",
                             "type": "image/png",
                             "density": "1.0"
                         },
                         {
-                            "src": "assets/android-icon-72x72.png",
+                            "src": "android-icon-72x72.png",
                             "sizes": "72x72",
                             "type": "image/png",
                             "density": "1.5"
                         },
                         {
-                            "src": "assets/android-icon-96x96.png",
+                            "src": "android-icon-96x96.png",
                             "sizes": "96x96",
                             "type": "image/png",
                             "density": "2.0"
                         },
                         {
-                            "src": "assets/android-icon-144x144.png",
+                            "src": "android-icon-144x144.png",
                             "sizes": "144x144",
                             "type": "image/png",
                             "density": "3.0"
                         },
                         {
-                            "src": "assets/android-icon-192x192.png",
+                            "src": "android-icon-192x192.png",
                             "sizes": "192x192",
                             "type": "image/png",
                             "density": "4.0"
