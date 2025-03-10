2833090a8a4c46cf38f17054276045f346998e3a
urapadmin <38838314+urapadmin@users.noreply.github.com>
2025-03-10 14:41:05 +0100

    #2892: sync plugins won't be loaded if explicitly deactivated

--- pluginpwastresstestdock/pwastresstestdock.py
+++ pluginpwastresstestdock/pwastresstestdock.py
@@ -1,7 +1,7 @@
 from werkzeug import datastructures
 
 import kioskstdlib
-import kioskdatetimelib
+import kioskdatetimelibx
 from dsd.dsd3 import DataSetDefinition
 from dsd.dsd3singleton import Dsd3Singleton
 from dsd.dsdview import DSDView
@@ -18,13 +18,15 @@ class PWAStressTestDock(RecordingWorkstation):
         dsd = Dsd3Singleton.get_dsd3()
         dsd_workstation_view = DSDView(dsd)
         dsd_workstation_view.apply_view_instructions({"config":
-                                                          {"format_ver": 3},
-                                                      "tables": ["include_tables_with_instruction('replfield_uuid')",
-                                                                 "include_tables_with_flag('filemaker_recording')",
-                                                                 "exclude_field('images', 'filename')",
-                                                                 "exclude_field('images', 'md5_hash')",
-                                                                 "exclude_field('images', 'image_attributes')"
-                                                                 ]})
+            {
+                "format_ver": 3
+            },
+            "tables": ["include_tables_with_instruction('replfield_uuid')",
+                       "include_tables_with_flag('filemaker_recording')",
+                       "exclude_field('images', 'filename')",
+                       "exclude_field('images', 'md5_hash')",
+                       "exclude_field('images', 'image_attributes')"
+                       ]})
         return dsd_workstation_view.dsd
 
     def upload_file(self, file: datastructures.FileStorage) -> bool:
